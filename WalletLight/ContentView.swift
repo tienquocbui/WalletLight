69100405b02f504118e87d141ba6ad336a15101c
tienquocbui <kelvinbui12345@gmail.com>
2025-02-10 07:03:16 +0100

    Initial Commit

---
+++ WalletLight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WalletLight
+//
+//  Created by Bùi Tiến Quốc on 10/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WalletLight/WalletLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletLightApp.swift
+//  WalletLight
+//
+//  Created by Bùi Tiến Quốc on 10/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct WalletLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
